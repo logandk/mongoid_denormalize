1e99724472329adf3bd2d8e16fdb806926b1c5f1
Sina Iman <sina.iman@gmail.com>
2011-10-23 07:42:30 -0400

    comment can't reference user, MongoDB doesn't support joins in this manner

--- spec/app/models/comment.rb
+++ spec/app/models/comment.rb
@@ -5,8 +5,6 @@ class Comment
   field :body
   
   embedded_in :post, :inverse_of => :comments
-  
-  denormalize :name, :from => :user
-  denormalize :email, :from => :user
+
   denormalize :created_at, :type => Time, :from => :post
 end
