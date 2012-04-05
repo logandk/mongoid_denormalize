befc119d1cac563ea36f881da7c8cc1224cdc142
Isaac Cambron <icambron@alum.mit.edu>
2012-04-05 06:36:46 -0400

    added :as option

--- spec/app/models/user.rb
+++ spec/app/models/user.rb
@@ -4,10 +4,13 @@ class User
   
   field :name
   field :email
+  field :nickname
   field :location, :type => Array
-  
+
   has_one :post
   has_many :comments
+  has_many :moderated_comments, :class_name => "Comment", :inverse_of => :moderator
   
   denormalize :name, :email, :location, :to => [:post, :comments]
+  denormalize :nickname, :to => :moderated_comments, as: :moderator
 end
